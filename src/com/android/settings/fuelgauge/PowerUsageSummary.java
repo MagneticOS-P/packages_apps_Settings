2c97b9a97f23a85e69c2caaf9beaeaf0064d7d5d
nelo360 <nelo360@gmail.com>
2019-02-25 16:16:15 +0100

    fix FC on battery summary settings
    Change-Id: Id51cb466c94ae9a64cd6bb9ab6406b7de6bc8df6

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -53,7 +53,6 @@ import com.android.settings.Utils;
 import com.android.settings.applications.LayoutPreference;
 import com.android.settings.core.SubSettingLauncher;
 import com.android.settings.dashboard.SummaryLoader;
-import com.android.settings.display.BatteryPercentagePreferenceController;
 import com.android.settings.fuelgauge.anomaly.Anomaly;
 import com.android.settings.fuelgauge.anomaly.AnomalyDetectionPolicy;
 import com.android.settings.fuelgauge.batterytip.BatteryTipLoader;
@@ -304,7 +303,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 KEY_BATTERY_TIP, (SettingsActivity) getActivity(), this /* fragment */, this /*
                 BatteryTipListener */);
         controllers.add(mBatteryTipPreferenceController);
-        controllers.add(new BatteryPercentagePreferenceController(context));
         return controllers;
     }
 
@@ -612,11 +610,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> niks = super.getNonIndexableKeys(context);
 
-                    final BatteryPercentagePreferenceController controller =
-                            new BatteryPercentagePreferenceController(context);
-                    if (!controller.isAvailable()) {
-                        niks.add(controller.getPreferenceKey());
-                    }
                     niks.add(KEY_BATTERY_SAVER_SUMMARY);
                     return niks;
                 }
